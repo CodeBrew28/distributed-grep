24454efc8d35109ad13ad8983ab9f714f3457a97
arshiam2 <arshiam2@illinois.edu>
2018-10-30 21:45:00 -0500

    chsnged port

--- grep-server/index.js
+++ grep-server/index.js
@@ -1,5 +1,5 @@
 var WebSocketServer = require("ws").Server;
-var wss = new WebSocketServer({ port: 8080 });
+var wss = new WebSocketServer({ port: 3000 });
 wss.on("connection", function (ws, req) {
     // const ip = req.headers['x-forwarded-for'].split(/\s*,\s*/)[0];
    console.info("websocket connection open on " + String(req.headers['x-forwarded-for']));
