232a99085e1a2f2cae47153a70cadd5f37220f42
arshiam2 <arshiam2@illinois.edu>
2018-10-31 11:10:32 -0500

    updated everything

--- grep-js-client/index.js
+++ grep-js-client/index.js
@@ -1,5 +1,5 @@
 var WebSocket = require("ws");
 ws = new WebSocket('ws://104.196.24.172:3000');
 ws.onopen = function (evt) {
-   ws.send("Hello!");
+   ws.send("Hello from node web socket client");
 };
