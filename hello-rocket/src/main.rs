ad867fcfbf07c9f5d303e7ee2d87b8e375a6eecd
arshiam2 <arshiam2@illinois.edu>
2018-10-31 00:29:48 -0500

    rocket

---
+++ hello-rocket/src/main.rs
@@ -0,0 +1,15 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+#[get("/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/hello", routes![hello])
+        .launch();
+}
